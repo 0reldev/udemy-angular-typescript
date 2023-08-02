c9c3f9bf71e29c61f206870cfc0e8d52ca0e3127
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-02 16:43:25 +0200

    Lesson about primitive types

---
+++ basics.ts
@@ -0,0 +1,13 @@
+// Primitives: number, string, boolean
+// More complexe types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number = 24;
+age = 12;
+let userName: string;
+userName = 'Aurélien';
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: null; // null is an undefined type
