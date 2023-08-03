13fed28860894fb31cffbdbd53b47fd2e12cdb51
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-03 14:20:49 +0200

    Lesson about generic types

--- basics.ts
+++ basics.ts
@@ -53,3 +53,17 @@ function print(value: any) {
     console.log(value);
 }
 // A function which doesn't return anything actually returns void.
+
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].spilt('');
